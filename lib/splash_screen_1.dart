53f15b050f9402af899a0adb8ddb056f8180ba44
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-08 00:41:27 +0700

    update mengubah lingkaran agar jadi ditengah

--- lib/splash_screen_1.dart
+++ lib/splash_screen_1.dart
@@ -6,18 +6,20 @@ class SplashScreen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 250,
-            height: 250,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.green,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 250,
+              height: 250,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.green,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
