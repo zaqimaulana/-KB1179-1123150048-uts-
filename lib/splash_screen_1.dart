945973b69171b3beb32a7b4cf384fc2995167805
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-08 13:39:50 +0700

    menambahkan button continue

--- lib/splash_screen_1.dart
+++ lib/splash_screen_1.dart
@@ -84,6 +84,35 @@ class SplashScreen1 extends StatelessWidget {
                 ),
               ],
             ),
+
+             const SizedBox(height: 20),
+
+            // Tombol Continue
+            Container(
+              child: SizedBox(
+                width: double.infinity,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // nanti diarahkan ke splash screen 2
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF00C853),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
